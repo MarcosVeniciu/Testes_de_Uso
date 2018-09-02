dd7a551e9db29256aa8c3834b6f4383a61ea1810
Marcos <marcos.barbalho@ufv.br>
2018-09-02 01:32:10 -0300

    w

--- teste01.c
+++ teste01.c
@@ -1,9 +1,13 @@
 int main(int argc, char const *argv[]) {
 int i, count=10;
   if (1>0) {
+<<<<<<< HEAD
     for (i = 0; i < count; i++) {
       /* code */
     }
+=======
+    
+>>>>>>> a
   }
   return 0;
 }
